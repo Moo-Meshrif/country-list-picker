e08ce71ee017a4615a1a6f6a66ebac1dffc95be0
Meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2025-05-30 16:38:09 +0300

    add phone validation fun

--- lib/src/support/country_picker_helper.dart
+++ lib/src/support/country_picker_helper.dart
@@ -4,6 +4,8 @@ import '../model/languages.dart';
 import 'countires_multi_languages_list.dart';
 
 class CountryPickerHelper {
+  
+  /// Get a country from its dial code
   static Countries getCountriesFromDialCode(String dialCode) {
     Map<String, dynamic> country = countriesMultiLanguagesList
         .firstWhere((element) => element['dialing_code'] == dialCode);
@@ -13,9 +15,12 @@ class CountryPickerHelper {
     );
   }
 
-  static Country getCountryFromDialCode(Languages language, String dialCode) {
-    Map<String, dynamic> country = countriesMultiLanguagesList
-        .firstWhere((element) => element['dialing_code'] == dialCode);
+  /// Get a country from its dial code
+  static Country? getCountryFromDialCode(Languages language, String dialCode) {
+    int index = countriesMultiLanguagesList
+        .indexWhere((element) => element['dialing_code'] == dialCode);
+    if (index == -1) return null;
+    Map<String, dynamic> country = countriesMultiLanguagesList[index];
     return Country(
       iso_3166_1_alpha2: country['iso_3166_1_alpha2'],
       iso_3166_1_alpha3: country['iso_3166_1_alpha3'],
@@ -32,5 +37,13 @@ class CountryPickerHelper {
       local_number_sample: country['local_number_sample'],
       flagUri: 'assets/flags/${country['iso_3166_1_alpha2'].toLowerCase()}.png',
     );
+  }
+
+  /// Validate a phone number based on the country's dial code.
+  static bool validatePhoneNumber(String phoneNumber, String dialCode) {
+    Country? country = getCountryFromDialCode(Languages.English, dialCode);
+    if (country == null) return false;
+    int length = phoneNumber.length;
+    return length == country.default_number_length;
   }
 }
